8a1d9c817e0156cfae8aa8cc51808e3df546856d
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-07-07 11:54:17 +0630

    install vue app and vue3-form-wizard

---
+++ multi-step/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <form-wizard step-size="xs" :start-index="1">
+    <tab-content title="Personal details">
+      My first tab content
+    </tab-content>
+    <tab-content title="Additional Info">
+      My second tab content
+    </tab-content>
+    <tab-content title="Last step">
+      Yuhuuu! This seems pretty damn simple
+    </tab-content>
+  </form-wizard>
+</template>
+
+<style scoped>
+</style>
